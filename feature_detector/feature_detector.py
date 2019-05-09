61ccfcc8202bcae987b78c4364870cb14c235835
Sunanda <rcsunanda@gmail.com>
2019-05-09 03:04:01 -0400

    Load images code

--- feature_detector/feature_detector.py
+++ feature_detector/feature_detector.py
@@ -1,7 +1,38 @@
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+from PIL import Image
+
+# Script params
+
+positive_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Positive_mini"
+negative_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Negative_mini"
+
+
+def read_imgs(dir):
+    imgs_list = []
+
+    directory = os.fsencode(dir)
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            path = os.path.join(dir, filename)
+            img = Image.open(path)
+            imgs_list.append(img)
+
+    return imgs_list
 
 
 def main():
-    print("Starting program")
+    print("Loading images")
+
+    positive_imgs_list = read_imgs(positive_imgs_dir)
+    negative_imgs_list = read_imgs(negative_imgs_dir)
+
+    # negative_imgs_list[0].show()
+
     print("Done")
 
 
