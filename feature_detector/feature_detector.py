bf96bdfb6947baca9f79206524867e6e5dc32659
Sunanda <rcsunanda@gmail.com>
2019-05-09 09:36:26 -0400

    Random forest, prediction

--- feature_detector/feature_detector.py
+++ feature_detector/feature_detector.py
@@ -1,4 +1,5 @@
 import os
+import time
 import matplotlib.pyplot as plt
 
 from skimage import io, exposure
@@ -7,15 +8,23 @@ from skimage.transform import resize
 from skimage.color import rgb2gray
 
 from sklearn.decomposition import PCA
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 # Script params
 
-positive_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Positive_mini"
-negative_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Negative_mini"
-pca_components = 5
+# positive_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Positive_mini"
+# negative_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Negative_mini"
+
+positive_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Positive_medium"
+negative_imgs_dir = "D:/Sunanda/PhD Study/Other Activities/NII_Hackathon/dataset/Negative_medium"
+
+pca_components = 0.99
 
 
 def prepare_datasets(positives, negatives):
@@ -105,13 +114,36 @@ def get_hog_imgs_list(imgs_list):
     return hog_imgs_list
 
 
+def predict(classifier, X, y_true, dataset_name):
+    print("Predicting on dataset: {}".format(dataset_name))
+
+    y_pred = classifier.predict(X)
+
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+
+    pd.set_option('display.float_format', '{:.4f}'.format)
+
+    print("Dataset: {} \naccuracy = {} \nprecision = {} \nrecall = {} \nf1 = {}"
+          .format(dataset_name, accuracy, precision, recall, f1))
+
+    return (y_pred,), (accuracy, precision, recall, f1)
+
 
 def main():
-    print("Loading images")
+    # --------------------------------------------------
+    print("\nLoading images\n")
+    t0 = time.time()
 
     positive_imgs_list = read_imgs(positive_imgs_dir)
     negative_imgs_list = read_imgs(negative_imgs_dir)
 
+    print("Loading images complete. time={:.2f} sec".format(time.time() - t0))
+
+
+    # --------------------------------------------------
 
     # Check
     img = positive_imgs_list[2]
@@ -124,20 +156,54 @@ def main():
     # plt.figure()
     # io.imshow(ds_hog_img, cmap=plt.cm.gray)
 
+
+    # --------------------------------------------------
+
+    print("\nComputing HOG images\n")
+    t0 = time.time()
+
     hog_positives = [get_hog_img(img)[1] for img in positive_imgs_list]
     hog_negatives = [get_hog_img(img)[1] for img in negative_imgs_list]
 
     hog_positives = [hog_img.flatten() for hog_img in hog_positives]
     hog_negatives = [hog_img.flatten() for hog_img in hog_negatives]
 
+    print("Computing HOG images complete. time={:.2f} sec".format(time.time() - t0))
+
     (X_train, y_train), (X_val, y_val), (X_test, y_test) = prepare_datasets(hog_positives, hog_negatives)
 
-    # Do PCA
+
+    # --------------------------------------------------
+
+    print("\nDoing PCA\n")
+    t0 = time.time()
+
     pca_model, X_train = do_pca(X_train, n_components=pca_components)
     unused_var, X_val = do_pca(X_val, pca_model=pca_model)
     unused_var, X_test = do_pca(X_test, pca_model=pca_model)
 
+    print("PCA complete. time={:.2f} sec".format(time.time() - t0))
+
     plt.show()
+
+
+    # --------------------------------------------------
+
+    print("\nTraining Random Forest\n")
+    t0 = time.time()
+
+    # Copy the RF code from IDS 2017 code
+    # After that, try ANN
+    # If that does not work, may have to look at a different feature reduction (KernelPCA, downsample) or not doing PCA and going for a CNN
+
+    clf = RandomForestClassifier(n_estimators=10, n_jobs=2, random_state=0)
+    clf.fit(X_train, y_train)
+
+    # --------------------------------------------------
+
+    print("\n------------ Predicting on data -------------\n")
+
+    (y_pred,), (accuracy, precision, recall, f1) = predict(clf, X_train, y_train, "Training set")
 
     print("Done")
 
